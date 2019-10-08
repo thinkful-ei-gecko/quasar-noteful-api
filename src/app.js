4144892e0aaefcead33fade65be8337a5e69a48a
Quasar Wei <quasar.wei@outlook.com>
2019-10-08 14:22:31 -0700

    add validate token

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+
+const logger = require('./logger');
 const { NODE_ENV } = require('./config');
 const foldersRouter = require('./folders/folders-router');
 const notesRouter = require('./notes/notes-router');
@@ -16,6 +18,18 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized Request' });
+  }
+  next();
+});
+
 
 app.use('/api/folders', foldersRouter);
 app.use('/api/notes', notesRouter);
